3dd47dc8619ffbbed8a38d6f9c7c9be822ef062a
Tardice <florent.bernar@outlook.fr>
2024-05-19 12:43:36 +0200

    ResetPassword Fonctionne (mail token etc )

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { login } from '../services/authService';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { login, resetPasswordRequest, resetPassword } from '../services/authService';
 import { useAuth } from '../context/authContext';
 import './Login.css';
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [resetEmail, setResetEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
   const [error, setError] = useState(null);
+  const [resetError, setResetError] = useState(null);
+  const [isResetRequest, setIsResetRequest] = useState(false);
+  const [isResetPassword, setIsResetPassword] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
   const { login: authLogin } = useAuth();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(location.search);
+    const token = queryParams.get('token');
+    if (token) {
+      setIsResetPassword(true);
+    }
+  }, [location.search]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(null); // Clear previous errors
     try {
       const response = await login({ email, password });
       console.log('Login successful:', response);
@@ -22,6 +37,33 @@ const LoginForm = () => {
       navigate('/home');
     } catch (error) {
       setError(error.message);
+    }
+  };
+
+  const handleResetRequest = async (e) => {
+    e.preventDefault();
+    setResetError(null); // Clear previous errors
+    try {
+      const response = await resetPasswordRequest(resetEmail);
+      console.log('Reset password request successful:', response);
+      alert(response.message);
+      setIsResetRequest(false);
+    } catch (error) {
+      setResetError(error.message);
+    }
+  };
+
+  const handleResetPassword = async (e) => {
+    e.preventDefault();
+    setResetError(null); // Clear previous errors
+    try {
+      const token = new URLSearchParams(window.location.search).get('token'); // Get token from URL
+      const response = await resetPassword({ token, newPassword });
+      console.log('Password reset successful:', response);
+      setIsResetPassword(false);
+      navigate('/home');
+    } catch (error) {
+      setResetError(error.message);
     }
   };
 
@@ -35,52 +77,112 @@ const LoginForm = () => {
           <div className="intro-content-wrapper">
             <h1 className="intro-title">Bienvenue sur Quackers!</h1>
             <p className="intro-text">
-              Connecte toi afin d'utiliser Quackers!
+              Connecte-toi afin d'utiliser Quackers!
             </p>
-          </div>
-          <div className="intro-section-footer">
           </div>
         </div>
         <div className="col-sm-6 col-md-5 form-section">
           <div className="login-wrapper">
-            <h2 className="login-title">Connecte toi</h2>
-            <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                <label htmlFor="email" className="sr-only">Email</label>
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  className="form-control"
-                  placeholder="Email"
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="form-group mb-3">
-                <label htmlFor="password" className="sr-only">Mot de passe</label>
-                <input
-                  type="password"
-                  name="password"
-                  id="password"
-                  className="form-control"
-                  placeholder="Password"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                />
-              </div>
-              <div className="d-flex justify-content-between align-items-center mb-5">
-                <input
-                  name="login"
-                  id="login"
-                  className="btn login-btn"
-                  type="submit"
-                  value="Connexion"
-                />
-                <a href="#!" className="forgot-password-link"> Mot de passe oublié?</a>
-              </div>
-            </form>
+            <h2 className="login-title">
+              {isResetRequest ? 'Réinitialiser le mot de passe' : isResetPassword ? 'Nouveau mot de passe' : 'Connecte-toi'}
+            </h2>
+            {isResetRequest ? (
+              <form onSubmit={handleResetRequest}>
+                <div className="form-group">
+                  <label htmlFor="resetEmail" className="sr-only">Email</label>
+                  <input
+                    type="email"
+                    name="resetEmail"
+                    id="resetEmail"
+                    className="form-control"
+                    placeholder="Email"
+                    value={resetEmail}
+                    onChange={e => setResetEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="d-flex justify-content-between align-items-center mb-5">
+                  <input
+                    name="resetRequest"
+                    id="resetRequest"
+                    className="btn login-btn"
+                    type="submit"
+                    value="Envoyer le lien de réinitialisation"
+                  />
+                  <button type="button" className="btn btn-link" onClick={() => setIsResetRequest(false)}>
+                    Annuler
+                  </button>
+                </div>
+              </form>
+            ) : isResetPassword ? (
+              <form onSubmit={handleResetPassword}>
+                <div className="form-group">
+                  <label htmlFor="newPassword" className="sr-only">Nouveau mot de passe</label>
+                  <input
+                    type="password"
+                    name="newPassword"
+                    id="newPassword"
+                    className="form-control"
+                    placeholder="Nouveau mot de passe"
+                    value={newPassword}
+                    onChange={e => setNewPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="d-flex justify-content-between align-items-center mb-5">
+                  <input
+                    name="resetPassword"
+                    id="resetPassword"
+                    className="btn login-btn"
+                    type="submit"
+                    value="Réinitialiser le mot de passe"
+                  />
+                </div>
+              </form>
+            ) : (
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="email" className="sr-only">Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="form-control"
+                    placeholder="Email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label htmlFor="password" className="sr-only">Mot de passe</label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    className="form-control"
+                    placeholder="Mot de passe"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                  />
+                </div>
+                <div className="d-flex justify-content-between align-items-center mb-5">
+                  <input
+                    name="login"
+                    id="login"
+                    className="btn login-btn"
+                    type="submit"
+                    value="Connexion"
+                  />
+                  <button type="button" className="btn btn-link" onClick={() => setIsResetRequest(true)}>
+                    Mot de passe oublié?
+                  </button>
+                </div>
+              </form>
+            )}
             {error && <div className="alert alert-danger">{error}</div>}
+            {resetError && <div className="alert alert-danger">{resetError}</div>}
           </div>
         </div>
       </div>
